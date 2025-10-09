e7eff107f7be4e8ba2892c51b14f7e1ddc64456b
Peca <peca@mycity.rs>
2025-10-09 12:33:08 +0200

    phpgate: adding rate-limit fot AI bots

--- src/deb/for-download/tools/phpgate/phpgate-agent-strings.php
+++ src/deb/for-download/tools/phpgate/phpgate-agent-strings.php
@@ -1,64 +1,83 @@
 <?php
-$phpgate_the_same_ip=false;
-if (isset($_SERVER['SERVER_ADDR']) && isset($_SERVER['REMOTE_ADDR'])) {
-    if ($_SERVER['REMOTE_ADDR']=="127.0.0.1" || $_SERVER['SERVER_ADDR'] == $_SERVER['REMOTE_ADDR']) $phpgate_the_same_ip=true;
+
+function phpgate_bot_detected($agent_string, $block_after) {
+    if ($block_after == -1) {
+        phpgate_cut_request();
+    }
+    if ($block_after > 0) {
+        phpgate_count_limit_bot($agent_string, $block_after);
+    }
+
 }
+
 if (isset($_SERVER['HTTP_USER_AGENT'])) {
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "MJ12bot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "SemrushBot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "ahrefs.com")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "AhrefsBot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "dotbot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "Dotbot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "DotBot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "DynatraceSynthetic")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "Buck/")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "PetalBot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "GrapeshotCrawler")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "YandexBot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "Seekport")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "BLEXBot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "ClaudeBot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "ChatGPT")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "GPTBot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "Amazonbot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "Applebot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "Mediatoolkitbot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "trendictionbot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "SirdataBot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "Go-http-client")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "Apache-HttpClient/")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "Python/")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "Python-urllib")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "python-requests")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "SeekportBot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "AspiegelBot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "IonCrawl")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "emailcollector")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "emailsiphon")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "Baiduspider")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "Bytespider")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "Barkrowler")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "OAI-SearchBot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "SentiBot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "Awario")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "cert-manager/v")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "Mail.RU_Bot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "RSiteAuditor")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "GuzzleHttp")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "Dalvik")!==false && isset($phpgate_agent_skip_dalvik)==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "FreshpingBot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "smartocto-bot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "Ruby")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "ImagesiftBot")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "Scrapy")!==false) phpgate_cut_request();
-    if (strpos($_SERVER['HTTP_USER_AGENT'], "SeznamBot")!==false) phpgate_cut_request();
+    // -1 means instant block
+    // 0 means do not block
+    // >0 means block after X requests in the same minute
+
+    $phpgate_agent_strings = array(
+        "ClaudeBot" => 3,
+        "ChatGPT" => 3,
+        "GPTBot" => 3,
+        "meta-externalagent" => 3,
+        "ahrefs.com" => 1,
+        "AhrefsBot" => 1,
+        "MJ12bot" => 1,
+        "SemrushBot" => 1,
+        "YandexBot" => 1,
+        "Amazonbot" => 1,
+        "Applebot" => 1,
+        "dotbot" => -1,
+        "Dotbot" => -1,
+        "DotBot" => -1,
+        "DynatraceSynthetic" => -1,
+        "Buck/" => -1,
+        "PetalBot" => -1,
+        "GrapeshotCrawler" => -1,
+        "Seekport" => -1,
+        "BLEXBot" => -1,
+        "Mediatoolkitbot" => -1,
+        "trendictionbot" => -1,
+        "SirdataBot" => -1,
+        "Go-http-client" => -1,
+        "Apache-HttpClient/" => -1,
+        "Python/" => -1,
+        "Python-urllib" => -1,
+        "python-requests" => -1,
+        "SeekportBot" => -1,
+        "AspiegelBot" => -1,
+        "IonCrawl" => -1,
+        "emailcollector" => -1,
+        "emailsiphon" => -1,
+        "Baiduspider" => -1,
+        "Bytespider" => -1,
+        "Barkrowler" => -1,
+        "OAI-SearchBot" => -1,
+        "SentiBot" => -1,
+        "Awario" => -1,
+        "cert-manager/v" => -1,
+        "Mail.RU_Bot" => -1,
+        "RSiteAuditor" => -1,
+        "GuzzleHttp" => -1,
+        "Dalvik" => -1,
+        "FreshpingBot" => -1,
+        "smartocto-bot" => -1,
+        "Ruby" => -1,
+        "ImagesiftBot" => -1,
+        "Scrapy" => -1,
+        "SeznamBot" => -1
+    );
+    if (isset($phpgate_agent_strings_override)) {
+        foreach ($phpgate_agent_strings_override as $agent_string => $block_after) {
+            $phpgate_agent_strings[$agent_string] = $block_after;
+        }
+    }
+    foreach ($phpgate_agent_strings as $agent_string => $block_after) {
+        if (strpos($_SERVER['HTTP_USER_AGENT'], $agent_string)!==false) phpgate_bot_detected($agent_string, $block_after);
+    }
+
     if (isset($phpgate_skip_empty_agent_string_check)==false && $_SERVER['HTTP_USER_AGENT'] == "" && $phpgate_the_same_ip==false) phpgate_cut_request("Your Browser Agent string is empty. Maybe your antivirus does it. Please turn it off if you want to view this site.\n");
     if (isset($phpgate_skip_empty_agent_string_check)==false && $_SERVER['HTTP_USER_AGENT'] == "-" && $phpgate_the_same_ip==false) phpgate_cut_request("Your Browser Agent string is empty. Maybe your antivirus does it. Please turn it off if you want to view this site.\n");
 } else {
     if (isset($phpgate_skip_empty_agent_string_check)==false && isset($_SERVER['REQUEST_URI']) && $phpgate_the_same_ip==false) phpgate_cut_request("Your Browser Agent string is empty. Maybe your antivirus does it. Please turn it off if you want to view this site.\n");
-}
-
-if (isset($_SERVER['REQUEST_URI'])) {
-    if (strpos($_SERVER['REQUEST_URI'], "_rstr_nocache")!==false) phpgate_cut_request();
 }
--- src/deb/for-download/tools/phpgate/phpgate-func.php
+++ src/deb/for-download/tools/phpgate/phpgate-func.php
@@ -1,212 +1,205 @@
 <?php
 
+// xmlrpc.php block
 if (substr($_SERVER['PHP_SELF'], -10)=='xmlrpc.php') {
-    /*
-    foreach ($_POST as $var => $val) {
-        if (strpos($var, "wp_getUsersBlogs")!==false) {
-            header("HTTP/1.1 404 Not Found");
-            exit;
+    if (!isset($phpgate_skip_xmlrpc_block)) {
+        foreach ($_POST as $var => $val) {
+            if (strpos($var, "wp_getUsersBlogs")!==false) {
+                // block requests to xmlrpc.php if POST wp_getUsersBlogs parameter is present
+                header("HTTP/1.1 404 Not Found");
+                exit;
+            }
         }
+    } else {
+        // block all requests to xmlrpc.php
+        phpgate_cut_request();
     }
-    */
-    phpgate_cut_request();
 }
 
-function phpgate_update_timings($value) {
-    global $phpgate_global_counter, $phpgate_global_ip_array, $phpgate_tolerance_time;
+// --- callback functions ---
+
+// callback function to update timings - alter the number of suspicious hits in $arr ($phpgate_current_ip_array) array
+function phpgate_update_timings($arr) {
+    global $phpgate_current_ip_counter, $phpgate_current_ip_array, $phpgate_ban_time;
     $unixtime=time();
-    if ($value===null) {
-        $phpgate_global_counter=1;
-        $phpgate_global_ip_array=array($unixtime=>1);
-        return $phpgate_global_ip_array;
+    if ($arr===null) {
+        $phpgate_current_ip_counter=1;
+        $phpgate_current_ip_array=array($unixtime=>1);
+        return $phpgate_current_ip_array;
     }
 
-    if (isset($value[$unixtime])) $value[$unixtime]=intval($value[$unixtime])+1;
-    else $value[$unixtime]=1;
+    if (isset($arr[$unixtime])) $arr[$unixtime]=intval($arr[$unixtime])+1; // increment counter
+    else $arr[$unixtime]=1;
 
-    $phpgate_global_counter=0;
-    foreach ($value as $time => $counter) {
-        if ($time<$unixtime-$phpgate_tolerance_time) {unset($value[$time]); continue;}
-        $phpgate_global_counter=$phpgate_global_counter+$counter;
+    $phpgate_current_ip_counter=0;
+    foreach ($arr as $time => $counter) {
+        if ($time<$unixtime-$phpgate_ban_time) {unset($arr[$time]); continue;} // remove expired timings
+        $phpgate_current_ip_counter+=$counter; // add counter to current ip counter
     }
-    $phpgate_global_ip_array=$value;
-    return $value;
+    $phpgate_current_ip_array=$arr;
+    return $arr;
 }
 
-function phpgate_add_banned($value) {
+// callback function to add banned IP to array
+function phpgate_add_banned($arr) {
     global $phpgate_ip, $phpgate_global_banned;
     $unixtime=time();
 
-    if ($value===null) {
+    if ($arr===null) {
         $phpgate_global_banned=array($phpgate_ip=>$unixtime);
         return $phpgate_global_banned;
     }
 
-    $value[$phpgate_ip]=$unixtime;
-    $phpgate_global_banned=$value;
+    $arr[$phpgate_ip]=$unixtime;
+    $phpgate_global_banned=$arr;
 
-    return $value;
+    return $arr;
 }
 
-function phpgate_clear_old_banned($value) {
-    global $phpgate_ip, $phpgate_global_banned_time;
+// callback function to remove expired banned IPs from array
+function phpgate_remove_expired_banned($arr) {
+    global $phpgate_ip, $phpgate_ban_time;
     $unixtime=time();
 
-    if ($value===null) return array();
+    if ($arr===null) return array();
 
-    foreach ($value as $ip => $time) {
-        if ($time<$unixtime-$phpgate_global_banned_time) unset($value[$ip]);
+    foreach ($arr as $ip => $time) {
+        if ($time<$unixtime-$phpgate_ban_time) unset($arr[$ip]);
     }
-    return $value;
+    return $arr;
 }
 
-function phpgate_unban_banned($value) {
+// callback function to remove banned IP from array
+function phpgate_unban_banned($arr) {
     global $phpgate_ip, $phpgate_global_banned;
-    if ($value===null) return array();
-    if (isset($value[$phpgate_ip])) unset($value[$phpgate_ip]);
-    $phpgate_global_banned=$value;
-    return $value;
+    if ($arr===null) return array();
+    if (isset($arr[$phpgate_ip])) unset($arr[$phpgate_ip]);
+    $phpgate_global_banned=$arr;
+    return $arr;
 }
 
+// --- end of callback functions ---
+
+
+// --- memcached helper functions ---
+
+// function to update memcached safely
+// parameters:
+// $key - key to update
+// $update_cb - callback function to update value
+// $ttl - time to live
 function phpgate_memcached_safe_update($key, $update_cb, $ttl=600) {
-    global $phpgate_memcached, $phpgate_debug;
+    global $phpgate_memcached; //, $phpgate_debug;
     $loop=0;
+    $cas = null;
     while (true) {
         $loop++;
-        if ($loop==10) break;
+        if ($loop==10) break; // if loop is 10, break
         $result_ok=true;
-        if (defined('Memcached::GET_EXTENDED')==false) $value = $phpgate_memcached->get($key, null, $cas);
-        else {
+        if (defined('Memcached::GET_EXTENDED')==false) {
+            $value = $phpgate_memcached->get($key, null, $cas);
+        } else {
             $r = $phpgate_memcached->get($key, null, Memcached::GET_EXTENDED);
-            if (isset($r['cas'])) $cas = $r['cas'];
-            else $result_ok=false;
-            if (isset($r['value'])) $value = $r['value'];
-            else $result_ok=false;
+            if (isset($r['cas'])) $cas = $r['cas']; // get cas value
+            else $result_ok=false; // if cas is not set, consider it as not found
+            if (isset($r['value'])) $value = $r['value']; // get value
+            else $result_ok=false; // if value is not set, consider it as not found
         }
-        //sleep(5);
-        if ($phpgate_memcached->getResultCode() == Memcached::RES_NOTFOUND || $result_ok==false) {
+
+        if ($result_ok==false || $phpgate_memcached->getResultCode() == Memcached::RES_NOTFOUND) {
+            // not found, add new value
             $value = call_user_func($update_cb, null);
             $phpgate_memcached->add($key, $value, $ttl);
             if ($phpgate_memcached->getResultCode() == Memcached::RES_SUCCESS) break;
         } else {
+            // found, update value
             $value = call_user_func($update_cb, $value);
-            $phpgate_memcached->cas($cas, $key, $value, $ttl);
+            if (defined('Memcached::GET_EXTENDED')==false || $cas==null) $phpgate_memcached->set($key, $value, $ttl);
+            else $phpgate_memcached->cas($cas, $key, $value, $ttl);
             if ($phpgate_memcached->getResultCode() == Memcached::RES_SUCCESS) break;
         }
-        if ($phpgate_debug) echo "loop<br />\n";
-        usleep(100000);
+        //if ($phpgate_debug) echo "loop<br />\n";
+        usleep(100000); // sleep for 0.1 seconds
     }
 }
 
-function phpgate_log_post_request($action) {
-    global $phpgate_global_ip_array, $phpgate_global_banned, $phpgate_ip, $phpgate_banned_key;
-    //$b="Found: $where = $key = ".print_r($val, true)."\n\n";
-    $b="Action: ".$action."\n";
-    $b.="Time: ".date("d M Y H:i:s")."\n";
-    if (isset($_SERVER['SERVER_NAME'])) $b.="Domain: ".$_SERVER['SERVER_NAME']."\n";
-    if (isset($_SERVER['REQUEST_URI'])) $b.="URL: ".$_SERVER['REQUEST_URI']."\n";
-    if (isset($_SERVER['HTTP_USER_AGENT'])) $b.="Agent: ".$_SERVER['HTTP_USER_AGENT']."\n";
-    $b.="IP: ".$phpgate_ip."\n";
-    $host=@gethostbyaddr($phpgate_ip);
-    $b.="Host: ".$host."\n";
-    $b.="IP array: ".print_r($phpgate_global_ip_array, true)."\n";
-    $b.="Banned key: ".$phpgate_banned_key."\n";
-    $b.="Banned array: ".print_r($phpgate_global_banned, true)."\n";
+// end of memcached helper functions
 
-    $b.="GET:\n";
-    $b.=print_r ($_GET, true);
-    $b.="\n\n";
-
-    $b.="POST:\n";
-    $b.=print_r ($_POST, true);
-    $b.="\n\n";
-
-    $b.="SERVER:\n";
-    $b.=print_r ($_SERVER, true);
-    $b.="\n\n";
-
-    $b.="COOKIE:\n";
-    $b.=print_r ($_COOKIE, true);
-
-    if (isset($_SERVER['CONTENT_TYPE']) && $_SERVER['CONTENT_TYPE']=="application/json") {
-        $json=file_get_contents("php://input");
-        $b.="JSON:\n".$json."\n";
-        $b.=print_r(json_decode($json), 1);
-        $b.="\n\n";
-    }
-    $b.="\n\n=========================================================================================\n\n";
-    
-    file_put_contents("/usr/share/phpgate/banned-log.txt", $b, FILE_APPEND);
-}
-
-if ($_SERVER['REMOTE_ADDR']!=="127.0.0.1" && $_SERVER['REMOTE_ADDR']!==$_SERVER['SERVER_ADDR']) {
+if ($phpgate_the_same_ip==false) {
     // main flow if it's request from outside
 
     //ini_set('display_errors',1); ini_set('display_startup_errors',1); error_reporting(-1);
-    $phpgate_global_banned_time=93600;
-    $phpgate_tolerance_time=93600;
-    $phpgate_debug=false;
-    $phpgate_max_fails=6;
 
+    // set default values
+    if (isset($phpgate_ban_time)==false) $phpgate_ban_time=86400; // 24 hours
+    if (isset($phpgate_extend_ban_time)==false) $phpgate_extend_ban_time=true;
+    if (isset($phpgate_max_retry)==false) $phpgate_max_retry=5;
+    if (isset($phpgate_find_time)==false) $phpgate_find_time=600;
+    // $phpgate_debug=false;
+    if (!class_exists('Memcached')) return;
     $phpgate_memcached = new Memcached();
     $phpgate_memcached->addServer('localhost', 11211);
 
-    $phpgate_ip=$_SERVER['REMOTE_ADDR'];
-    // if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) $phpgate_ip=$_SERVER['HTTP_CF_CONNECTING_IP'];
-    $phpgate_key='phpgate_ip_'.$phpgate_ip;
+    if (isset($phpgate_ip)==false) $phpgate_ip=$_SERVER['REMOTE_ADDR'];
+    if (isset($phpgate_key)==false) $phpgate_key='phpgate_ip_'.$phpgate_ip;
 
-    $phpgate_ip_last_dot=strrpos($phpgate_ip, '.');
-    if ($phpgate_ip_last_dot==false) {
-        $phpgate_ip_last_dot=strrpos($phpgate_ip, ':');
-        $phpgate_ip=substr($phpgate_ip, 0, $phpgate_ip_last_dot);
-        $phpgate_ip=str_replace(':0:0:0', '', $phpgate_ip);
-        $phpgate_ip_last_dot=strrpos($phpgate_ip, ':');
+    if (isset($phpgate_banned_key)==false) {
+        $phpgate_ip_last_dot=strrpos($phpgate_ip, '.');
+        if ($phpgate_ip_last_dot==false) {
+            $phpgate_ip_last_dot=strrpos($phpgate_ip, ':');
+            $phpgate_ip=substr($phpgate_ip, 0, $phpgate_ip_last_dot);
+            $phpgate_ip=str_replace(':0:0:0', '', $phpgate_ip);
+            $phpgate_ip_last_dot=strrpos($phpgate_ip, ':');
+        }
+        $phpgate_banned_key='phpgate_banned_'.substr($phpgate_ip, 0, $phpgate_ip_last_dot);
     }
-    $phpgate_banned_key='phpgate_banned_'.substr($phpgate_ip, 0, $phpgate_ip_last_dot);
 
     if (isset($_GET['remove_me_from_blacklist'])==false) {
         // *************************** main phpgate processing - log suspicious request or block request if IP is already banned ***************************
         $phpgate_global_banned=$phpgate_memcached->get($phpgate_banned_key);
         if (isset($phpgate_global_banned[$phpgate_ip])) {
             // IP is found in ban list
-            if (isset($phpgate_global_banned[$phpgate_ip]) && $phpgate_global_banned[$phpgate_ip] < time()-$phpgate_global_banned_time) {
-                // last hit was before 26 hours, unban IP
-                phpgate_memcached_safe_update($phpgate_banned_key, 'phpgate_clear_old_banned', 86400); // time to unban IP
+            if ($phpgate_global_banned[$phpgate_ip] < time()-$phpgate_ban_time) {
+                // last hit was before $phpgate_ban_time, unban IP
+                phpgate_memcached_safe_update($phpgate_banned_key, 'phpgate_remove_expired_banned', $phpgate_ban_time); // time to unban IP
             } else {
-                // last his was in last 26 hours, so add 24 hours to time_counter and keep it blocked
-                phpgate_memcached_safe_update($phpgate_banned_key, 'phpgate_add_banned', 86400);
+                if ($phpgate_extend_ban_time==true) {
+                    // last his was in last $phpgate_ban_time seconds, so add $phpgate_ban_time seconds to time_counter and keep it blocked
+                    phpgate_memcached_safe_update($phpgate_banned_key, 'phpgate_add_banned', $phpgate_ban_time);
+                }
+                // block request
                 phpgate_cut_request("Too many wrong login attempts - or - bot traffic detected - your IP is banned.<br />\nClick <script>document.write('<a href=\"".$_SERVER['PHP_SELF']."?'+'remove'+'_me_from_blacklist=1\">here</a>')</script> to remove you from blacklist.");
             }
         }
 
         if ($phpgate_should_count==true) {
             // alter the number of suspicious hits
-            phpgate_memcached_safe_update($phpgate_key, 'phpgate_update_timings', 600);
+            phpgate_memcached_safe_update($phpgate_key, 'phpgate_update_timings', $phpgate_find_time);
             
+            /*
             if ($phpgate_debug) {
                 echo "final: "; print_r($phpgate_memcached->get($phpgate_key)); echo "<br />\n";
-                echo "count: ".$phpgate_global_counter."<br />\n";
+                echo "count: ".$phpgate_current_ip_counter."<br />\n";
             }
+            */
             
-            if ($phpgate_global_counter>=$phpgate_max_fails) {
-                // maximum number of suspicious hits reached (4 hits found, fifth is just happening), ban IP, add 24 hours to time_counter, block request
-                phpgate_memcached_safe_update($phpgate_banned_key, 'phpgate_add_banned', 86400);
+            if ($phpgate_current_ip_counter>$phpgate_max_retry) {
+                // maximum number of suspicious hits reached (five hits found, sixth is just happening), ban IP, add 24 hours to time_counter, block request
+                phpgate_memcached_safe_update($phpgate_banned_key, 'phpgate_add_banned', $phpgate_ban_time);
                 phpgate_cut_request("Too many wrong login attempts - or - bot traffic detected - your IP is banned now.<br />\nClick <script>document.write('<a href=\"".$_SERVER['PHP_SELF']."?'+'remove'+'_me_from_blacklist=1\">here</a>')</script> to remove you from blacklist.");
                 // phpgate_log_post_request('ban');
             }
         }
     } else {
         // *** $_GET['remove_me_from_blacklist'] - manual removing from blacklist ***
-        phpgate_memcached_safe_update($phpgate_banned_key, 'phpgate_unban_banned', 86400);
+        phpgate_memcached_safe_update($phpgate_banned_key, 'phpgate_unban_banned', $phpgate_ban_time);
         $phpgate_memcached->delete($phpgate_key);
         // phpgate_log_post_request('unban');
-        if ($phpgate_debug) echo "unbanned<br />\n";
+        // if ($phpgate_debug) echo "unbanned<br />\n";
     }
+
     if (isset($phpgate_memcached)) unset($phpgate_memcached);
-    if (isset($phpgate_ip)) unset($phpgate_ip);
-    if (isset($phpgate_key)) unset($phpgate_key);
-    if (isset($phpgate_unban_banned)) unset($phpgate_unixtime);
-    if (isset($phpgate_banned_key)) unset($phpgate_banned_key);
-    if (isset($phpgate_should_count)) unset($phpgate_should_count);
-    if (isset($phpgate_global_counter)) unset($phpgate_global_counter);
+    foreach ($GLOBALS as $key => $value) {
+        if (strpos($key, 'phpgate_')!==false) unset($GLOBALS[$key]);
+    }
 }
