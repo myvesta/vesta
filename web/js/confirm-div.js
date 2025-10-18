1aa099bdd388b80958f39fd2f971de819c3fd3e8
Peca <peca@mycity.rs>
2025-10-19 00:19:48 +0200

    Enhance confirm dialog functionality by adding support for textarea and listbox input types.

--- web/js/confirm-div.js
+++ web/js/confirm-div.js
@@ -29,7 +29,7 @@ function showConfirmDiv(title, contentHtml, type = 'confirm', variableLabel = ''
     ConfirmDivType = type;
     if (type == 'confirm' && selectedButton == '') selectedButton = 'no';
     if (type == 'notify' && buttonLabels != '') $('#confirm-div-button-notify').html(buttonLabels);
-    if ((type == 'input' || type == 'confirm') && buttonLabels != '') {
+    if ((type == 'input' || type == 'confirm' || type == 'textarea') && buttonLabels != '') {
         if (buttonLabels.includes('/')) {
             buttonLabels = buttonLabels.split('/');
             $('#confirm-div-button-yes').html(buttonLabels[0]);
@@ -44,13 +44,15 @@ function showConfirmDiv(title, contentHtml, type = 'confirm', variableLabel = ''
     $('#confirm-div-buttons-confirm').css('display','none');
     $('#confirm-div-buttons-notify').css('display','none');
     $('#confirm-div-content-input').css('display','none');
+    $('#confirm-div-content-textarea').css('display','none');
+    $('#confirm-div-content-listbox').css('display','none');
 
     $('#confirm-div-title').html(title);
     $('#confirm-div-content').html(contentHtml);
     $('#confirm-div').fadeIn();
 
 
-    if (type == 'confirm' || type == 'input') {
+    if (type == 'confirm' || type == 'input' || type == 'textarea' || type == 'listbox') {
         $('#confirm-div-buttons-confirm').css('display','flex');
         if (type == 'confirm') {
             if (selectedButton == 'no') {
@@ -74,6 +76,26 @@ function showConfirmDiv(title, contentHtml, type = 'confirm', variableLabel = ''
         $('#confirm-div-content-input-variable').attr('placeholder', variableValue);
         $('#confirm-div-content-input-variable').focus();
     }
+    if (type == 'textarea') {
+        $('#confirm-div-content-textarea').css('display','block');
+        $('#confirm-div-content-textarea-label').html(variableLabel);
+        $('#confirm-div-content-textarea-variable').attr('name', variableName);
+        $('#confirm-div-content-textarea-variable').val(variableValue);
+        $('#confirm-div-content-textarea-variable').attr('placeholder', variableValue);
+        $('#confirm-div-content-textarea-variable').focus();
+    }
+    if (type == 'listbox') {
+        $('#confirm-div-content-listbox').css('display','block');
+        $('#confirm-div-content-listbox-label').html(variableLabel);
+        $('#confirm-div-content-listbox-variable').attr('name', variableName);
+        $('#confirm-div-content-listbox-variable').focus();
+        if (Array.isArray(variableValue)) {
+            for (let i = 0; i < variableValue.length; i++) {
+                $('#confirm-div-content-listbox-variable').append('<option value="'+variableValue[i].key+'">'+variableValue[i].value+'</option>');
+            }
+        }
+        $('#confirm-div-content-listbox-variable').val(selectedValue);
+    }
 }
 
 function hideConfirmDiv(event) {
@@ -86,7 +108,17 @@ document.addEventListener('DOMContentLoaded', function () {
     //showConfirmDiv('Confirm', 'Are you sure you want to continue?', 'confirm', '', '', '', '', 'yes', 'TESTFUNCTION');
     //showConfirmDiv('Notification!!!', 'You have a new notification!!!', 'notify', '', '', '', '', '', 'TESTFUNCTION', 'OK !!!');
     //showConfirmDiv('Notification!!!', 'You have a new notification!!!', 'input', 'Enter name', 'name', 'Some name', '', 'OK !!!', 'TESTFUNCTION', 'OK/Cancel');
-
+    //showConfirmDiv('Notification!!!', 'You have a new notification!!!', 'textarea', 'Enter name', 'name', 'Some name', '', 'OK !!!', 'TESTFUNCTION', 'OK/Cancel');
+    
+    /*
+    let options = [
+        { key: "1", value: "Option 1" },
+        { key: "2", value: "Option 2" },
+        { key: "3", value: "Option 3" }
+      ];
+    showConfirmDiv('Notification!!!', 'You have a new notification!!!', 'listbox', 'Select an option', 'option', options, '3', 'OK !!!', 'TESTFUNCTION', 'OK/Cancel');
+    */
+   
     $(document).on('keydown', function(e) {
         if (ConfirmDivOpened) {
             if (ConfirmDivType == 'input') {
